0572a54331eb5eb573c0e5a146bfed8e65d9b8de
Ben Greaves <bsgreaves@gmail.com>
2017-01-31 17:07:56 -0600

    Started JS. First function working, second need a fix.

--- main.js
+++ main.js
@@ -1,1 +1,26 @@
-Placeholder
+// Create an array that contains the letters of the alphabet
+var alphabet = "abcdefghijklmnopqrstuvwxyz".split("");
+console.log(alphabet);
+var output = "";
+
+// NOTE TO SELF, FIX THIS LOGIC
+// for (var i = 0; i < alphabet.length; i++) {
+// 	output += alphabet[i];
+// 	if (output.length === 3 || output.length % 4 === 0) {
+// 		output += " ";
+// 	} else {
+
+// 	}
+//     console.log(output);
+// }
+
+
+// The stackLetter function should accept the array as the sole argument
+// function stackLetters (array) {
+//      for (var i = 0; i < array.length; i++) {
+//      	console.log(array += array[i]);
+// 	}
+// }
+
+// Invoke the function and pass in the array
+//stackLetters(alphabet)
