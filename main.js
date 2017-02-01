450309a9c106284b5c8946f8ff6cfd08324eede8
Ben Greaves <bsgreaves@gmail.com>
2017-01-31 22:17:55 -0600

    Finished the functions. Assignment complete. Ready for feedback.

--- main.js
+++ main.js
@@ -1,26 +1,35 @@
-// Create an array that contains the letters of the alphabet
+// Creates an array that contains the letters of the alphabet
 var alphabet = "abcdefghijklmnopqrstuvwxyz".split("");
 console.log(alphabet);
 var output = "";
 
-// NOTE TO SELF, FIX THIS LOGIC
-// for (var i = 0; i < alphabet.length; i++) {
-// 	output += alphabet[i];
-// 	if (output.length === 3 || output.length % 4 === 0) {
-// 		output += " ";
-// 	} else {
+//A function that prints a stack of letters to the array
+function stackLetters (array) {
+     for (var i = 0; i < array.length; i++) {
+     	output += array[i];
+     	console.log(output);
+	}
+}
 
-// 	}
-//     console.log(output);
-// }
+//A function that does the same, but adds a space after each three letter group. Logic:
+//If arr.leng === 3, add a space.
+//After that, if arr.leng-3 % 4, add a space.
 
+function stackLettersByThrees (array) {
+     for (var i = 0; i < array.length; i++) {
+		output += array[i];
+		if (output.length === 3) {
+			output += " ";
+			console.log(output);
+		} else if ((output.length - 3) % 4 === 0) {
+			output += " ";
+			console.log(output);
+		} else {
+			console.log(output);
+		}
+	}
+}
 
-// The stackLetter function should accept the array as the sole argument
-// function stackLetters (array) {
-//      for (var i = 0; i < array.length; i++) {
-//      	console.log(array += array[i]);
-// 	}
-// }
-
-// Invoke the function and pass in the array
-//stackLetters(alphabet)
+stackLetters(alphabet);
+output = "";
+stackLettersByThrees(alphabet);
