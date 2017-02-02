0264f47efcf1b9213db7984281d82fdf319a8092
Ben Greaves <bsgreaves@gmail.com>
2017-02-01 18:30:44 -0600

    Removed comments

--- main.js
+++ main.js
@@ -1,19 +1,13 @@
-// Creates an array that contains the letters of the alphabet
 var alphabet = "abcdefghijklmnopqrstuvwxyz".split("");
 console.log(alphabet);
 var output = "";
 
-//A function that prints a stack of letters to the array
 function stackLetters (array) {
      for (var i = 0; i < array.length; i++) {
      	output += array[i];
      	console.log(output);
 	}
 }
-
-//A function that does the same, but adds a space after each three letter group. Logic:
-//If arr.leng === 3, add a space.
-//After that, if arr.leng-3 % 4, add a space.
 
 function stackLettersByThrees (array) {
      for (var i = 0; i < array.length; i++) {
