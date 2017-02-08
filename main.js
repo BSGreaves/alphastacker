05571b759a7ac12258de8fa533012a553f24f35c
Ben Greaves <bsgreaves@gmail.com>
2017-02-07 21:17:03 -0600

    Indented code properly

--- main.js
+++ main.js
@@ -2,14 +2,14 @@ var alphabet = "abcdefghijklmnopqrstuvwxyz".split("");
 var output = "";
 
 function stackLetters (array) {
-     for (var i = 0; i < array.length; i++) {
-     	output += array[i];
-     	console.log(output);
+	for (var i = 0; i < array.length; i++) {
+		output += array[i];
+		console.log(output);
 	}
 }
 
 function stackLettersByThrees (array) {
-     for (var i = 0; i < array.length; i++) {
+	for (var i = 0; i < array.length; i++) {
 		output += array[i];
 		if (output.length === 3) {
 			output += " ";
